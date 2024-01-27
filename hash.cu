3c37ae8e1e43277d205ca7370e636be2e68a317d
huanglei <huanglei@shanghaitech.edu.cn>
2024-01-27 20:42:50 +0800

    feat: sanity check and funcion

--- hash.cu
+++ hash.cu
@@ -15,70 +15,34 @@ struct hashTableEntry{
     // int value; no value is needed
 };
 
-//miller rabin prime test
-bool isPrime(int n){
-    if(n == 2) return true;
-    if(n % 2 == 0) return false;
-    int d = n - 1;
-    while(d % 2 == 0) d /= 2;
-    for(int i = 0;i < 10;++i){
-        int a = rand() % (n - 1) + 1;
-        int t = d;
-        int y = 1;
-        while(t > 0){
-            if(t & 1) y = 1ll * y * a % n;
-            a = 1ll * a * a % n;
-            t >>= 1;
+/*The following hash function is adapted from https://github.com/easyaspi314/xxhash-clean/blob/master/xxhash32-ref.c */
+static uint32_t const PRIME32_1 = 0x9E3779B1U;   /* 0b10011110001101110111100110110001 */
+static uint32_t const PRIME32_2 = 0x85EBCA77U;   /* 0b10000101111010111100101001110111 */
+static uint32_t const PRIME32_3 = 0xC2B2AE3DU;   /* 0b11000010101100101010111000111101 */
+static uint32_t const PRIME32_4 = 0x27D4EB2FU;   /* 0b00100111110101001110101100101111 */
+static uint32_t const PRIME32_5 = 0x165667B1U;   /* 0b00010110010101100110011110110001 */
+
+struct HashFunc{
+    int seed;
+    int tableSize;
+    /*https://github.com/easyaspi314/xxhash-clean/blob/86a04ab3f01277049a23f6c9e2c4a6c174ff50c4/xxhash32-ref.c#L97 */
+    __host__ __device__ int operator ()(int key) {
+        uint32_t ret = PRIME32_5 + seed;
+        uint8_t const* p = reinterpret_cast<uint8_t const*>(&key);
+        for(int i = 0;i < 4;++i){
+            ret += (*p++) * PRIME32_3;
+            ret = (ret << 17) | (ret >> 15);
+            ret *= PRIME32_4;
         }
-        bool flag = false;
-        if(y != 1){
-            while(d != n - 1){
-                if(y == n - 1){
-                    flag = true;
-                    break;
-                }
-                y = 1ll * y * y % n;
-                d <<= 1;
-            }
-            if(!flag) return false;
-        }
-    }
-    return true;
-}
-
-int randomPrime(){
-    int n = rand() % 1000000 + 1000000;
-    while(!isPrime(n)) ++n;
-    return n;
-}
-
-inline int hash(int key, int a,int b,int tableSize){
-    return (1ll * key  % tableSize * a + b) % tableSize;
-}
-
-
-class HashFunc{
-public:
-    virtual __host__ __device__ int operator ()(int key) {
-        return NOT_A_INDEX;
+        ret ^= ret >> 15;
+        ret *= PRIME32_2;
+        ret ^= ret >> 13;
+        ret *= PRIME32_3;
+        ret ^= ret >> 16;
+        return ret % tableSize;
     };
 };
 
-class HashFuncAffine: public HashFunc{
-    int a,b,tableSize;
-public:
-    HashFuncAffine(int a,int b,int c):a(a),b(b),tableSize(c){}
-    __host__ __device__ int operator ()(int key) override {
-        return (1ll * key  % tableSize * a + b) % tableSize;
-    }
-};
-
-class HashFuncBit : public HashFunc{
-public:
-    __host__ __device__ int operator ()(int key) override {
-        return 0;
-    }
-};
 
 __global__ void insertItem(hashTableEntry* d_table, int key, HashFunc f1,HashFunc f2,int* retval){
     *retval = 0;
@@ -119,12 +83,12 @@ __global__ void lookupItem(hashTableEntry* d_table, int key, HashFunc f1, HashFu
     *retval = NOT_A_INDEX;
 }
 
-bool validation(){
-    std::cout << "start sanity check" << std::endl;
-    std::mt19937_64 rng(10);
+bool validation(int tableSize_ = 1 << 25, int test_  = 1 << 19){
+    std::cout << "[Sanity Check] start sanity check" << "tableSize=" << tableSize_ << " test=" << test_ << std::endl;
+    std::mt19937_64 rng(123);
     std::uniform_int_distribution<int> dist(0,1e9);
-    int tableSize = 2000;
-    int test = 25;
+    int tableSize = tableSize_;
+    int test = test_;
     std::vector<int> keys;
     std::unordered_set<int> keySet;
     for(int i = 0;i < test;++i){
@@ -136,37 +100,37 @@ bool validation(){
     }
     hashTableEntry* d_table;
     cudaMalloc(&d_table,sizeof(hashTableEntry) * tableSize);
-    const int MAX_RETRY = 15;
+    const int MAX_RETRY = 30;
     int retries = MAX_RETRY;
 retry:
     cudaMemset(d_table,0xff,sizeof(hashTableEntry) * tableSize);
-    int a1 = randomPrime(), b1 = randomPrime();
-    int a2 = a1 + 2, b2 = b1 + 2;
-    HashFuncAffine f1{a1,b1,tableSize},f2{a2,b2,tableSize};
+    int a1 = dist(rng), a2 = dist(rng);
+    HashFunc f1{a1,tableSize},f2{a2,tableSize};
     std::map<std::pair<int,int>,int> dup; 
     for(int key:keys){
-        int h1 = hash(key,a1,b1,tableSize);
-        int h2 = hash(key,a2,b2,tableSize);
+        int h1 = f1(key);
+        int h2 = f2(key);
         dup[{h1,h2}]++;
         // std:: cout << key << " " << h1 << " " << h2 << std::endl;
         if(dup[{h1,h2}] == 3){
-            std::cout << "hash collision detected. h1 = " << h1 << " h2 = " << h2 <<  std::endl;
+            std::cout << "[Sanity Check] inherent collision detected. h1 = " << h1 << " h2 = " << h2 <<  std::endl;
             for(auto key2 : keys){
-                int h1_ = hash(key2,a1,b1,tableSize);
-                int h2_ = hash(key2,a2,b2,tableSize);
+                int h1_ = f1(key2); // hash(key2,a1,b1,tableSize);
+                int h2_ = f2(key2); // hash(key2,a2,b2,tableSize);
                 if(h1 == h1_ && h2 == h2_)
-                    std::cout << key2 << " " << h1 << " " << h2 << std::endl;
+                    std::cout << "[Sanity Check] key,h1,h2 = " <<  key2 << " " << h1 << " " << h2 << std::endl;
             }
             if(retries--){
-                std::cout << "retrying..." << std::endl;
+                std::cout << "[Sanity Check] retrying..." << std::endl;
                 goto retry;
             }else{
-                std::cout << "failed after " +  std::to_string(MAX_RETRY) + " retries. aborting..." << std::endl;
+                std::cout << "[Sanity Check] failed after " +  std::to_string(MAX_RETRY) + " retries. aborting..." << std::endl;
                 cudaFree(d_table);
                 return false;
             }
         }
     }
+    std::cout<<"[Sanity Check] no inherent hash collision detected." << std::endl;
     //insert test
     for(int key : keys){
         int* d_retval;
@@ -176,20 +140,20 @@ retry:
         int retval;
         cudaMemcpy(&retval,d_retval,sizeof(int),cudaMemcpyDeviceToHost);
         if(retval == 1){
-            std::cout << "insertion failed (or reconstruction needed)" << std::endl;
+            std::cout << "[Sanity Check] insertion failed (or reconstruction needed)" << std::endl;
 
             if(retries--){
-                std::cout << "retrying..." << std::endl;
+                std::cout << "[Sanity Check] retrying..." << std::endl;
                 goto retry;
             }else{
-                std::cout << "failed after " +  std::to_string(MAX_RETRY) + " retries. aborting..." << std::endl;
+                std::cout << "[Sanity Check] failed after " +  std::to_string(MAX_RETRY) + " retries. aborting..." << std::endl;
                 cudaFree(d_table);
                 return false;
             }
             cudaFree(d_table);
         }
     }
-    std::cout << "passed insert test." << std::endl;
+    std::cout << "[Sanity Check] passed insert test." << std::endl;
     //lookup test
     for(int key : keys){
         int* d_retval;
@@ -200,12 +164,12 @@ retry:
         cudaMemcpy(&retval,d_retval,sizeof(int),cudaMemcpyDeviceToHost);
         cudaFree(d_retval);
         if(retval == NOT_A_INDEX){
-            std::cout << "lookup failed (false negative)" << std::endl;
+            std::cout << "[Sanity Check] lookup failed (false negative)" << std::endl;
             cudaFree(d_table);
             return false;
         }
     }
-    std::cout << "passed lookup test. no false negative" << std::endl;
+    std::cout << "[Sanity Check] passed lookup test. no false negative" << std::endl;
     //lookup test
     for(int i = 0;i < test; ++i){
         int key;
@@ -220,24 +184,25 @@ retry:
         cudaMemcpy(&retval,d_retval,sizeof(int),cudaMemcpyDeviceToHost);
         cudaFree(d_retval);
         if(retval != NOT_A_INDEX){
-            std::cout << "lookup failed (false positive)" << std::endl;
+            std::cout << "[Sanity Check] lookup failed (false positive)" << std::endl;
             cudaFree(d_table);
             return false;
         }
     }
-    std::cout << "passed lookup test. no false positive" << std::endl;
-    std::cout << "passed all sanity tests!" << std::endl;
+    std::cout << "[Sanity Check] passed lookup test. no false positive" << std::endl;
+    std::cout << "[Sanity Check] passed all sanity tests!" << std::endl;
     //release resource
     cudaFree(d_table);
     return true;
 }
 
 int main(){
-    std::cout<< "start 5 rounds of sanity check" << std::endl;
+    const int sanityCheck = 10;
+    std::cout<< "[Sanity Check] start " +  std::to_string(sanityCheck) + " rounds of sanity check" << std::endl;
     int success = 0;
-    for(int i = 0;i < 1;++i){
-        success += validation();
+    for(int i = 1;i <= sanityCheck;++i){
+        success += validation(1 << 25 , 1 << int(10 + 1. * i / sanityCheck * (20 - 10)));
     }
-    std::cout << "passed " << success << " rounds out of 5 sanity check(s)" << std::endl;
+    std::cout << "[Sanity Check] passed " << success << " rounds out of " + std::to_string(sanityCheck) + " sanity check(s)" << std::endl;
     return 0;
 }
