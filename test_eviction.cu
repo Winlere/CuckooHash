25f2268be14e898ff588cee28707a5f9666198a0
huanglei <huanglei@shanghaitech.edu.cn>
2024-01-29 23:02:32 +0800

    feat: task4 script

--- test_eviction.cu
+++ test_eviction.cu
@@ -2,17 +2,20 @@
 #include "time.hpp"
 #include "helper.cuh"
 #include <iostream>
+#include <cmath>
 #include <vector>
 const int BLOCKSIZE = 512;
 int maxRetry = 50;
 
-int main()
+int main(int argc,char **argv)
 {
+    uint32_t seed = argc >= 2 ? atoi(argv[1]) : 0;
+    double percentageS = argc >= 3 ? atof(argv[2]) : -1;
     TIME_INIT;
     const uint32_t tableSize = 1 << 25;
     const uint32_t testMaxSize = 1 << 24;
-    const uint32_t seed1 = 114514;
-    const uint32_t seed2 = 191981;
+    uint32_t seed1 = 114514 + seed;
+    uint32_t seed2 = 191981 + seed;
     const int range = 1 << 30;
     HashFunc f1, f2;
     hashTableEntry *d_hashTable = nullptr;
@@ -29,9 +32,12 @@ int main()
     int testTableSize = 1.4 * testMaxSize;
     for (int maxMove = 1; maxMove <= 125; ++maxMove)
     {
-        f1.seed = 115838;
+        if(percentageS > 0){
+            maxMove = (int) (percentageS * std::log2(testTableSize));
+        }
+        f1.seed = 115838 + seed;
         f1.tableSize = testTableSize;
-        f2.seed = 193305;
+        f2.seed = 193305 + seed;
         f2.tableSize = testTableSize;
     retry_entry:
         int testSize = 1 << 24;
@@ -46,10 +52,15 @@ int main()
             ++maxRetry;
             f1 = {seed1 + maxRetry, (unsigned) testTableSize};
             f2 = {seed2 + maxRetry, (unsigned) testTableSize};
-            std::cout << "retrying..." << std::endl;
+            // std::cout << "retrying..." << std::endl;
             goto retry_entry;
         }
-        std::cout << "maxMove,elapsed_μs,valid | " << maxMove << "," << elapsed_μs << "," << valid << std::endl;
+        if(percentageS < 0)
+            std::cout << "maxMove,elapsed_μs,valid | " << maxMove << "," << elapsed_μs << "," << valid << std::endl;
+        else{
+            std::cout << elapsed_μs / 1e6 << ' ' << 1.0 * testMaxSize / elapsed_μs << std::endl;
+            break;
+        }
     }
     cudaFree(d_keys);
     cudaFree(d_retvals);
