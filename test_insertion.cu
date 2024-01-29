5277eef8a4dc57c0b573c227ade9bd2b487764d9
huanglei <huanglei@shanghaitech.edu.cn>
2024-01-29 22:25:33 +0800

    feat: task1

--- test_insertion.cu
+++ test_insertion.cu
@@ -3,11 +3,15 @@
 #include "helper.cuh"
 #include <iostream>
 #include <vector>
+#include <string>
 const int BLOCKSIZE = 512;
 int maxRetry = 50;
 
-int main()
+int main(int argc, char **argv)
 {
+    int seed = argc >= 2 ? std::stoi(std::string(argv[1])) : 0;
+    int targetS = argc >= 3 ? std::stoi(std::string(argv[2])) : -1;
+
     TIME_INIT;
     const uint32_t tableSize = 1 << 25;
     const uint32_t testMaxSize = 1 << 24;
@@ -29,9 +33,11 @@ int main()
 
     for (uint32_t s = 11; s <= 24; ++s)
     {
-        f1.seed = 115838 << 10;
+        if (targetS != -1 && s != targetS)
+            continue;
+        f1.seed = 115838 << 10 | seed;
         f1.tableSize = 4194304;
-        f2.seed = 193305 << 10;
+        f2.seed = 193305 << 10 | seed;
         f2.tableSize = 4194304;
     retry_entry:
         int testSize = 1 << s;
@@ -49,7 +55,10 @@ int main()
             std::cout << "retrying..." << std::endl;
             goto retry_entry;
         }
-        std::cout << "testsize,elapsed_μs,valid | " << testSize << "," << elapsed_μs << "," << valid << std::endl;
+        if(argc == 3){
+            std::cout << elapsed_μs/1e6 << " " << 1.0 * testSize / elapsed_μs << std::endl;
+        }else
+            std::cout << "testsize,elapsed_μs,valid | " << testSize << "," << elapsed_μs << "," << valid << std::endl;
     }
     cudaFree(d_keys);
     cudaFree(d_retvals);
